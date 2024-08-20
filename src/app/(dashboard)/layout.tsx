e52e7777b9be1141c93d1c58c1c89196ec196e43
nason <yunyoungsik91@gmail.com>
2024-08-20 18:04:34 +0900

    [20240820]31:08까지 완료

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Menu from '@/components/Menu';
+import Navbar from '@/components/Navbar';
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">School0Sik</span>
+        </Link>
+
+        <Menu />
+      </div>
+
+      {/* right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between p-4">
+      {/* search bar */}
+      <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+        <Image src="/search.png" alt="search" width={14} height={14} />
+        <input type="text" placeholder="Search..." className='w-[200px] p-2 bg-transparent outline-none' />
+      </div>
+
+      {/* icons and user */}
+      <div className="flex items-center gap-6 justify-end w-full">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/message.png" alt="message" width={20} height={20} />
+        </div>
+
+        <div className="relative bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/announcement.png" alt="announcement" width={20} height={20} />
+          <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">
+            1
+          </div>
+        </div>
+
+        <div className="flex flex-col">
+          <span className="text-xs leading-3 font-medium">0-Sik</span>
+          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+        </div>
+
+        <Image
+          src="/avatar.png"
+          alt="avatar"
+          width={36}
+          height={36}
+          className="rounded-full"
+        ></Image>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
